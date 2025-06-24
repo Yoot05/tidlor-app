7f6eb42b029919514df6249f00b1831961bab415
Yoot05 <yoot_5@hotmail.com>
2025-06-24 13:37:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.co.th"
           target="_blank"
           rel="noopener noreferrer"
         >
